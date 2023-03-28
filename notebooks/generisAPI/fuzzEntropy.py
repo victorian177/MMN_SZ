75c0438ceaf3ac7f0e6f7d3f704efefdba359027
emmanuel-olateju <olatejuemmanuel@gmail.com>
2023-03-28 21:42:40 +0100

    FuzzyEntropy
    self developed algorithm for fuzzy entropy tested, results not thoroughly investigated, but looking okay, 2D data specific version yet to be written

--- notebooks/generisAPI/fuzzEntropy.py
+++ notebooks/generisAPI/fuzzEntropy.py
@@ -1,48 +1,117 @@
 import numpy as np
+from scipy.spatial.distance import cdist
+import time
 
-class gaussianMembershipfunction:
-    def __init__(self,std,mean=0):
-        self.c = self.mean = mean
-        self.sigma = self.std = std
-
-    def fuzzyValue(self,x):
-        return np.exp(-0.5*((x-self.mean)/self.std)**2)
+def sigmoid(x,r):
+    assert isinstance(r,tuple), 'When Fx = "Sigmoid", r must be a two-element tuple.'
+    y = 1/(1 + np.exp((x-r[1])/r[0]))
+    return y  
+def default(x,r):   
+    assert isinstance(r,tuple), 'When Fx = "Default", r must be a two-element tuple.'
+    y = np.exp(-(x**r[1])/r[0])
+    return y     
+def modsampen(x,r):
+    assert isinstance(r,tuple), 'When Fx = "Modsampen", r must be a two-element tuple.'
+    y = 1/(1 + np.exp((x-r[1])/r[0]))
+    return y    
+def gudermannian(x,r):
+    if r <= 0:
+        raise Exception('When Fx = "Gudermannian", r must be a scalar > 0.')
+    y = np.arctan(np.tanh(r/x))    
+    y = y/np.max(y)    
+    return y    
+def linear(x,r):    
+    if r == 0 and x.shape[0]>1:    
+        y = np.exp(-(x - np.min(x))/np.ptp(x))
+    elif r == 1:
+        y = np.exp(-(x - np.min(x)))
+    elif r == 0 and x.shape[0]==1:   
+        y = 0
+    else:
+        print(r)
+        raise Exception('When Fx = "Linear", r must be 0 or 1')
+    return y
 
 class fuzzEntropy:
-    def __init__(self,window_size,dissimilarity_index,membership_function):
+    def __init__(self,window_size,dissimilarity_index,membership_function='linear'):
         self.m = self.window_size = window_size
         self.r = self.dissimilarity_index = dissimilarity_index
-        self.mu = self.membership_function = membership_function
+        self.mu = self.membership_function = globals()[membership_function.lower()]
     
     def __computeFuzzyMatrix(self,x,m):
-        N = x.shape[0]
+        if x.ndim == 1:
+            N = x.shape[0]
+            # x = (x - x.mean())/x.std()
+            Xm = np.array([x[i:i+m-1].tolist() for i in range(0,N-m)])
+            dm = cdist(Xm,Xm,'euclidean')
+            dm = self.mu(dm,self.r)
+            phim = np.sum(dm,axis=1)/(N-m+1)
+            # dm = dm<=self.r
+            # phim = np.sum(dm.astype(np.int0),axis=1)/(N-m+1)
 
-        X = np.array([x[i:i+m-1].tolist() for i in range(0,N-m)])
-        U0 = np.repeat(np.sum(X,axis=1),m-1).reshape(X.shape[0],X.shape[1])
+        # U0 = np.repeat(np.sum(Xm,axis=1),m-1).reshape(Xm.shape[0],Xm.shape[1])
+        # Xm = Xm - np.repeat(np.sum(Xm,axis=1),m-1).reshape(Xm.shape[0],Xm.shape[1])
+        # Xm_1 = Xm_1 - np.repeat(np.sum(Xm_1,axis=1),m-2).reshape(Xm_1.shape[0],Xm_1.shape[1])
+        # dm = np.zeros((len(Xm),len(Xm)))
+        # dm_1 = np.zeros((len(Xm),len(Xm)))
+        # for i in range(N-self.d):
+        #     for j in range(N-self.d):
+        #         if i != j:
+        #             dm[i,j]=abs(Xm[i,:]-Xm[j,:]).max()
+        #             dm_1[i,j]=abs(Xm_1[i,:]-Xm_1[j,:]).max()
+        # Dm = self.mu(self.r).fuzzyValue(dm)
+        # Dm_1 = self.mu(self.r).fuzzyValue(dm_1)
+        # phim = (1/(N-m+1)) * np.sum(dm,axis=1)
+        # phim1 = (1/(N-m-1)) * np.sum(dm_1,axis=1)
 
-        Xm = X - U0
-
-        d=list()
-        for i in range(N-m):
-            tmp=list()
-            for j in range(N-m):
-                if i != j:
-                    tmp.append((X[i,:]-X[j,:]).tolist())
-            d.append(tmp)
-        dm = np.array(d).max(axis=2)
-
-        Dm = self.mu(self.r).fuzzyValue(dm)
-
-        phim = (1/(N-m-1)) * np.sum(Dm,axis=1)
-
+        # return np.sum(dm)/(N-self.d)
         return phim
     
-    def fuzzyEntropyCompute(self,x):
+    def _fuzzyEntropyCompute(self,x):
         N = x.shape[0]
         phim = self.__computeFuzzyMatrix(x,self.m)
         phim1 = self.__computeFuzzyMatrix(x,self.m+1)
 
-        psim = (1/(N-self.m)) * np.sum(phim,axis=0)
-        psim1 = (1/(N-self.m)) * np.sum(phim1,axis=0)
+        psim = (1/(N-self.m+1)) * np.sum(phim,axis=0)
+        psim1 = (1/(N-self.m+2)) * np.sum(phim1,axis=0)
+
+        with np.errstate(divide='ignore', invalid='ignore'):
+            fuzz = np.log(psim)-np.log(psim1)
+            # fuzz = np.log(psim)
+            # fuzz = psim
+        return fuzz
+    
+def fuzzEntropy2D(x,window_size,dissimilarity_index,membership_function=gaussianMembershipfunction):
+        fuzzyent = fuzzEntropy(window_size,dissimilarity_index,membership_function)
+
+        res = np.empty(x.shape[0])
+        for i in range(x.shape[0]):
+            res[i] = fuzzyent._fuzzyEntropyCompute(x[i,:])
+        return res.mean()
+
+class chatGPTfuzzyEntropy:
+    def __init__(self):
+        pass
+
+    def _embedding(self,data, m, d):
+        N = len(data)
+        return np.array([data[i:i+m] for i in range(N-d+1)])
+
+    def _prob_matrix(self,embedding):
+        dists = cdist(embedding, embedding, 'euclidean')
+        return np.array([np.sum(dists[i] <= dists[i,-1]) - 1 for i in range(len(embedding))])/float(len(embedding)-1)
+
+    def fuzzy_entropy(self,data, m, d, r):
+        embedding = self._embedding(data, m, d)
         
-        return np.log(psim)-np.log(psim1)
+        npoints = len(embedding)
+        patten_match = np.zeros((npoints,npoints))
+
+        for i in range(npoints):
+            for j in range(npoints):
+                if (max(abs(embedding[i]-embedding[j]))) <= r:
+                    patten_match[i,j]=1
+
+        p = np.sum(self._prob_matrix(embedding[patten_match==1]))/float(npoints*(npoints-1))
+
+        return -np.log(p)
--- notebooks/generisAPI/processing.py
+++ notebooks/generisAPI/processing.py
@@ -2,6 +2,7 @@ import numpy as np
 import mne
 import math
 import EntropyHub
+from generisAPI.fuzzEntropy import *
 
 def xtract_phase_data(phase,data):
     res=dict()
@@ -12,7 +13,8 @@ def xtract_phase_data(phase,data):
     return res
 
 class trial_epoching:
-    def __init__(self,dummy=0):
+    def __init__(self,mode='min'):
+        self.mode=mode
         pass
 
     def fit_transform(self,X):
@@ -20,10 +22,14 @@ class trial_epoching:
             print('beta',X.shape)
             return X
         else:
-            for i in range(15,0,-1):
+            no_epochs=[]
+            for i in range(15,1,-1):
                 if X.shape[1]%i == 0:
-                    x=X.reshape(i,X.shape[0],int(X.shape[1]/i))
-                    break
+                    no_epochs.append(i)
+            if self.mode == 'min':
+                x=X.reshape(min(no_epochs),X.shape[0],int(X.shape[1]/min(no_epochs)))
+            else:
+                x=X.reshape(max(no_epochs),X.shape[0],int(X.shape[1]/max(no_epochs)))
             return x
 
 class trial_averaging:
@@ -53,8 +59,11 @@ class stft:
         return res
 
 class fuzzEnt:
-    def __init__(self,m):
+    def __init__(self,m,r=1,mode='self',muFunction='linear'):
         self.m = m
+        self.r = r
+        self.mu = muFunction
+        self.mode = mode
 
     def fit_transform(self,X):
         if X.ndim == 3:
@@ -65,15 +74,33 @@ class fuzzEnt:
                         if e.shape[-1]%i==0 and e.shape[-1]/i<=126:
                             break
                     cnt=int(e.shape[-1]/i)
-                    ent = EntropyHub.FuzzEn2D(e[:,0:cnt],self.m)
+                    if self.mode=='hub':
+                        ent = EntropyHub.FuzzEn2D(e[:,0:cnt],self.m)
+                    else:
+                        ent = fuzzEntropy2D(e[:,0:cnt],self.m,self.r,self.mu)
                     for c in range(1,i):
-                        ent += EntropyHub.FuzzEn2D(e[:,c*cnt:(c+1)*cnt],self.m)
+                        if self.mode=='hub':
+                            ent += EntropyHub.FuzzEn2D(e[:,c*cnt:(c+1)*cnt],self.m)
+                        else:
+                            ent += fuzzEntropy2D(e[:,c*cnt:(c+1)*cnt],self.m,self.r,self.mu)
                 else:
-                    ent=EntropyHub.FuzzEn2D(e,self.m,Lock=False)
+                    if self.mode=='hub':
+                        ent=EntropyHub.FuzzEn2D(e,self.m,Lock=False)
+                    else:
+                        ent += fuzzEntropy2D(e,self.m,self.r,self.mu)
                 res=np.append(res,ent)
             return res
         elif X.ndim == 2:
-            return EntropyHub.FuzzEn2D(X,self.m,Lock=False)
+            if self.mode=='hub':
+                return EntropyHub.FuzzEn2D(X,self.m,Lock=False)
+            else:
+                return fuzzEntropy2D(X,self.m,self.r,self.mu)
+        elif X.ndim == 1:
+            if self.mode=='self':
+                return fuzzEntropy(self.m,self.r,self.mu)._fuzzyEntropyCompute(X)
+            elif self.mode=='hub':
+                return EntropyHub.FuzzEn(X,self.m)
+
 
 def phase_trials_processor(pipeline,phase):
     res=[]
@@ -87,4 +114,5 @@ def all_subjects(pipeline,data):
     for s in data:
         res[s] = phase_trials_processor(pipeline,data[s]['eeg_data'])
         # print(s)
+    print("done")
     return res
